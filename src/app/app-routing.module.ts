5c6f7615b79ae3cb4f9ddbc92a34567709454979
A-Pradeep-Raj <mail2pradeepraj@gmail.com>
2021-07-16 12:16:56 +0530

    commit to master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+const routes: Routes = [{ path: 'users', component: UserListComponent },
+{ path: 'adduser', component: UserFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
